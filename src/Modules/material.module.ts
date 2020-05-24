6c6ca553f1a1e1bd45f5110fc8043aac372ad031
dan <maina.dan.ndirangu@gmail.com>
2020-05-24 15:57:01 +0300

    :sparkles: add application toolbar

---
+++ src/Modules/material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+@NgModule({
+  imports: [
+    MatToolbarModule,
+    MatSidenavModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatSidenavModule
+  ],
+  providers: []
+})
+
+export class MaterialModule { }
