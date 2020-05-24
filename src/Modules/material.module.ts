50989989bd0b12dbf215a0105fb73c93826b510e
dan <maina.dan.ndirangu@gmail.com>
2020-05-24 16:52:34 +0300

    :sparkles: add side panel

--- src/Modules/material.module.ts
+++ src/Modules/material.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   imports: [
     MatToolbarModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule
   ],
   exports: [
     MatToolbarModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule
   ],
   providers: []
 })
