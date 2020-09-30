ea77dcdabaf09f14caea925e298cc34e44380619
dan <maina.dan.ndirangu@gmail.com>
2020-09-30 20:31:06 +0300

    :construction: add navigation drawer on the left of the app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
+
 <template>
   <div>
-    <router-view></router-view>
+    <div class="col-3">
+      <layout></layout>
+    </div>
+    <div class="col-9">
+<!--      <router-view></router-view>-->
+    </div>
   </div>
 </template>
 
 <script>
+import Layout from "@/views/layout/Layout";
 export default {
   name: 'App',
 
   data: () => ({
     //
   }),
+  components: {
+    Layout
+  }
 };
 </script>
