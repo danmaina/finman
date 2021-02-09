8239307d6e0bf67161e9f122eb96f8fffb131d62
dan <maina.dan.ndirangu@gmail.com>
2021-02-10 00:08:51 +0300

    :construction: Add application navigation

--- src/main/java/com/spectrum/gradient/controllers/MainPage.java
+++ src/main/java/com/spectrum/gradient/controllers/MainPage.java
@@ -1,11 +1,56 @@
 package com.spectrum.gradient.controllers;
 
-import lombok.AllArgsConstructor;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
 
 @Component
 @FxmlView("MainPage.fxml")
 public class MainPage {
+
+    @FXML
+    AnchorPane overviewPane;
+
+    @FXML
+    AnchorPane reportsPane;
+
+    @FXML
+    AnchorPane budgetPane;
+
+    @FXML
+    AnchorPane settingsPane;
+
+    @FXML
+    public void showOverviewPane() {
+        overviewPane.setVisible(true);
+        reportsPane.setVisible(false);
+        budgetPane.setVisible(false);
+        settingsPane.setVisible(false);
+    }
+
+    @FXML
+    public void showBudgetPane() {
+        overviewPane.setVisible(false);
+        budgetPane.setVisible(true);
+        reportsPane.setVisible(false);
+        settingsPane.setVisible(false);
+    }
+
+    @FXML
+    public void showReportsPane() {
+        overviewPane.setVisible(false);
+        budgetPane.setVisible(false);
+        reportsPane.setVisible(true);
+        settingsPane.setVisible(false);
+    }
+
+    @FXML
+    public void showSettingsPane() {
+        overviewPane.setVisible(false);
+        budgetPane.setVisible(false);
+        reportsPane.setVisible(false);
+        settingsPane.setVisible(true);
+    }
 
 }
